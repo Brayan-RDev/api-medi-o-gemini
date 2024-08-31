bf3c17404c860119f6c2e628fea08967f67cd4ef
Brayan <codinomekillerkira@gmail.com>
2024-08-31 01:59:41 -0300

    configurando variveis de ambiente

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,6 +1,8 @@
+import { env } from "../env";
+
 export async function processingImageToMeasurement(image: string) {
   const { GoogleGenerativeAI } = require("@google/generative-ai");
-  const genAI = new GoogleGenerativeAI("AIzaSyBe4G--9qc5OwMKjzV_fxE3e3raDWepwlY");
+  const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   const prompt = `Retorne apenas os dígitos registrados no medidor, Entre os digitos existe uma linha pequena e vertical preta para separar os digitos reais, Concentre-se em reconhecer dígitos completos e bem definidos`
